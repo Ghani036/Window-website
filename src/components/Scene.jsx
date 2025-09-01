361d61c9c163d8f34baaf65d2d00dbd21a59a0e5
Ghani36 <ghani.uetm@gmail.com>
2025-09-01 15:19:10 +0500

    Logo reveal changed

--- src/components/Scene.jsx
+++ src/components/Scene.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { useScroll, Preload } from "@react-three/drei";
+import { useScroll, Preload, Stars } from "@react-three/drei";
 import VideoBackground from "./VideoBackground";
 import LogoVideoHUD from "./LogoVideoHUD";
 import CameraRig from "./CameraRig";
@@ -13,6 +13,7 @@ export default function Scene() {
       <LogoVideoHUD scroll={scroll} />
       <CameraRig scroll={scroll} />
       <Preload all />
+      <Stars saturation={false} />
     </>
   );
 }
